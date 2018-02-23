dbb0bb62f51623f1bec435228c8178768bee7ffb
Ethan Steininger <ethan@Ethans-MacBook-Pro.local>
2018-02-23 15:53:30 -0500

    Some python

---
+++ objects/Google.py
@@ -0,0 +1,7 @@
+class Google:
+    def __init__(self):
+        pass
+
+    def getFAQs(self):
+        url = 'https://sheets.googleapis.com/v4/spreadsheets/'+spreadsheetID+'/developerMetadata/'+metaID
+        return ''
